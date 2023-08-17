4e4aa18e2ee3f6717a62974841a353c5f6c386d6
sinchandasgupta <sinchandsgpt9@gmail.com>
2023-08-18 04:12:40 +0530

    Maintabs: Add motion underline

--- components/MainTabs.tsx
+++ components/MainTabs.tsx
@@ -2,6 +2,7 @@
 import * as Tabs from "@radix-ui/react-tabs";
 import { type Sitetabs } from "@/siteConfig";
 import { useRouter, usePathname } from "next/navigation";
+import { motion } from "framer-motion";
 
 interface MainTabsProps {
   tabs: Sitetabs;
@@ -15,12 +16,18 @@ export default function MainTabs({ tabs }: MainTabsProps) {
       {tabs.map((tab) => (
         <Tabs.Trigger
           key={tab.name}
-          className="py-4 text-lg font-medium cursor-pointer text-cyan-300/60 border-b border-transparent data-[state=active]:text-cyan-300/90  data-[state=active]:border-b-cyan-300/40 data-[state=active]:border-b data-[state=active]:bg-gradient-to-br data-[state=active]:from-cyan-950/10 data-[state=active]:to-cyan-950/30 hover:bg-cyan-950/30 data-[state=active]:font-semibold tracking-wider"
+          className="relative lg:py-4 py-3 text-lg font-medium cursor-pointer text-cyan-300/60 border-b border-transparent data-[state=active]:text-cyan-300/90 data-[state=active]:bg-gradient-to-br data-[state=active]:from-cyan-950/10 data-[state=active]:to-cyan-950/30 hover:bg-cyan-950/30 data-[state=active]:font-semibold tracking-wider"
           value={tab.name}
           data-state={pathname === tab.path ? "active" : "inactive"}
           onClick={() => router.push(tab.path)}
         >
           {tab.name}
+          {pathname === tab.path && (
+            <motion.div
+              className="absolute bottom-0 left-0 w-full h-[1.5px] bg-cyan-300/50"
+              layoutId="underline"
+            />
+          )}
         </Tabs.Trigger>
       ))}
     </Tabs.List>
