b402c79efef9d3b57811877b945b313724db186c
sinchandasgupta <sinchandsgpt9@gmail.com>
2023-08-25 06:23:06 +0530

    Remove unnecessary type interface

--- components/TabContent.tsx
+++ components/TabContent.tsx
@@ -2,16 +2,11 @@
 import { cn } from "@/lib/utils";
 import * as Tabs from "@radix-ui/react-tabs";
 
-interface TabContentProps {
-  children: React.ReactNode;
-  value: string;
-}
-
 export default function TabContent({
   children,
   className,
   ...props
-}: TabContentProps & React.ComponentPropsWithoutRef<typeof Tabs.Content>) {
+}: React.ComponentPropsWithoutRef<typeof Tabs.Content>) {
   return (
     <Tabs.Content className={cn("w-full", className)} {...props}>
       {children}
