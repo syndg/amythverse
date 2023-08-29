b5096988b7790bfcb67da5e975c749d8d4a94c8f
sinchandasgupta <sinchandsgpt9@gmail.com>
2023-08-30 03:37:36 +0530

    Styles update

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -6,7 +6,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <>
-      <h1 className="uppercase text-center text-2xl font-bold text-cyan-100/90 mb-4">
+      <h1 className="uppercase text-center text-2xl font-bold text-cyan-100/90 mb-2">
         Services
       </h1>
       <p className="py-3 px-5 lg:p-4 lg:text-xl text-cyan-100/70 leading-5  mb-5 font-sans border-l-[1.8px] border-r-[1.8px] border-cyan-400/50">
