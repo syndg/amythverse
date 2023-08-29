c0ebc9d870c4f9cefc9fda2f66401d6c3fda3d6b
sinchandasgupta <sinchandsgpt9@gmail.com>
2023-08-30 03:45:28 +0530

    Styles update

--- app/(routes)/layout.tsx
+++ app/(routes)/layout.tsx
@@ -1,5 +1,4 @@
 import Image from "next/legacy/image";
-import Link from "next/link";
 import MainTabs from "@/components/MainTabs";
 import { Card } from "@/components/ui/card";
 import { tabs } from "@/siteConfig";
@@ -21,12 +20,9 @@ export default function HomeLayout({
         />
       </div>
       <header className="grid place-content-center p-4 md:p-5 border-b z-30 text-cyan-300/90 border-b-cyan-300/40 bg-cyan-950/20 backdrop-blur-lg">
-        <Link
-          href="/"
-          className="text-2xl font-bold font-mono tracking-widest md:text-3xl lg:text-4xl"
-        >
+        <h1 className="text-2xl font-bold font-mono tracking-widest md:text-3xl lg:text-4xl">
           AMYTHVERSE
-        </Link>
+        </h1>
       </header>
       <div className="flex justify-center p-4">
         <Card>
